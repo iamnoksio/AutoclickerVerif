dddba483015571c620fdee03a3979c72345bc53a
Noksio <42706907+iamnoksio@users.noreply.github.com>
2022-10-18 16:01:59 -0400

    remove usless class

--- src/main/java/fr/ctlserv/william/autoclick/CheckRunnable.java
+++ src/main/java/fr/ctlserv/william/autoclick/CheckRunnable.java
@@ -4,40 +4,32 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import fr.ctlserv.william.autoclick.customevents.AutoclickAlertEvent;
-
 public class CheckRunnable extends BukkitRunnable {
 	private int timeBetweenAlerts;
 	private int maxCps;
-	private String AlertMessage;
-	private String Perm;
+	private String alertMessage;
+	private String permission;
 	
 	public CheckRunnable(int secondes, int maxCps, String AlertMessage, String Perm){
 		this.timeBetweenAlerts = secondes;
 		this.maxCps = maxCps;
-		this.AlertMessage = AlertMessage;
-		this.Perm = Perm;
+		this.alertMessage = AlertMessage;
+		this.permission = Perm;
 	}
 	
 	public void run() {
 		for (PlayerWrapper wp : PlayerWrapper.players.values()){
 			int ping = wp.getPlayer().getPing();
             double tps = Math.round(Bukkit.spigot().getTPS()[0] * 1e2)/1e2;
-            int AntiLag = (int)((20.0D - tps) * 2.0D);
-            AntiLag += ping / 50;
-            if ((wp.clicks[0] >= maxCps + AntiLag) && (wp.lastAlert + timeBetweenAlerts * 1000L < System.currentTimeMillis())) {
-            	AutoclickAlertEvent event = new AutoclickAlertEvent(wp.pseudo, wp.clicks[0], ping , tps );
-            	Bukkit.getServer().getPluginManager().callEvent(event);
+            int antiLag = (int)((20.0D - tps) * 2.0D);
+            antiLag += ping / 50;
+            if ((wp.clicks[0] >= maxCps + antiLag) && (wp.lastAlert + timeBetweenAlerts * 1000L < System.currentTimeMillis())) {
             	wp.lastAlert = System.currentTimeMillis();
-            	if (!event.isCancelled()){
-	            	for (Player toAlert : Bukkit.getOnlinePlayers()){
-	            		if (toAlert.hasPermission(Perm)){
-	            			toAlert.sendMessage(AlertMessage.replace("%username", wp.pseudo)
-	            		  .replace("%cps", String.valueOf(wp.clicks))
-	            		  .replace("%ms", String.valueOf(ping))
-	            		  .replace("%tps", String.valueOf(tps))
-	            		  .replace("&", "§"));
-	            		}
+	            for (Player toAlert : Bukkit.getOnlinePlayers()){
+	            	if (toAlert.hasPermission(permission)){
+	            		toAlert.sendMessage(alertMessage.replace("%username", wp.pseudo).replace("%cps", String.valueOf(wp.clicks)).replace("%ms", String.valueOf(ping))
+	            				.replace("%tps", String.valueOf(tps))
+	            				.replace("&", "§"));
 	            	}
 	                wp.nombreAlertesAutoClick += 1; 
             	}
